5c39127714b2050f02c3022a23c6035ef775d752
MohammedPrince <mohammed.beng@gmail.com>
2026-09-22 12:17:56 +0200

    Fixes and Updates: Notifications APIs, Result status v09

--- resources/views/admin/reports.blade.php
+++ resources/views/admin/reports.blade.php
@@ -653,8 +653,47 @@
                 </table>
 
                 @if ($report['recent_activity']->hasPages())
-                    <div class="pagination-wrapper">
-                        {{ $report['recent_activity']->onEachSide(1)->links() }}
+                    @php
+                        $activityPaginator = $report['recent_activity'];
+                        $startPage = max(1, $activityPaginator->currentPage() - 1);
+                        $endPage = min($activityPaginator->lastPage(), $activityPaginator->currentPage() + 1);
+                    @endphp
+
+                    <div class="pagination-wrapper report-pagination">
+                        <p class="report-pagination-summary">
+                            Showing {{ $activityPaginator->firstItem() }} to {{ $activityPaginator->lastItem() }} of
+                            {{ $activityPaginator->total() }} results
+                        </p>
+
+                        <nav aria-label="Recent student activity pagination">
+                            <ul class="report-pagination-list">
+                                <li>
+                                    @if ($activityPaginator->onFirstPage())
+                                        <span class="is-disabled" aria-disabled="true">Previous</span>
+                                    @else
+                                        <a href="{{ $activityPaginator->previousPageUrl() }}" rel="prev">Previous</a>
+                                    @endif
+                                </li>
+
+                                @foreach ($activityPaginator->getUrlRange($startPage, $endPage) as $page => $url)
+                                    <li>
+                                        @if ($page === $activityPaginator->currentPage())
+                                            <span class="is-active" aria-current="page">{{ $page }}</span>
+                                        @else
+                                            <a href="{{ $url }}">{{ $page }}</a>
+                                        @endif
+                                    </li>
+                                @endforeach
+
+                                <li>
+                                    @if ($activityPaginator->hasMorePages())
+                                        <a href="{{ $activityPaginator->nextPageUrl() }}" rel="next">Next</a>
+                                    @else
+                                        <span class="is-disabled" aria-disabled="true">Next</span>
+                                    @endif
+                                </li>
+                            </ul>
+                        </nav>
                     </div>
                 @endif
 
